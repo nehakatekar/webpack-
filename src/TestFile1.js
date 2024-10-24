9632264eb3366f7dea9a5b8258a78cdc3dccad75
neha <nehakatekar230@gmail.com>
2024-10-24 15:42:53 +0530

    bugFix 1

--- src/TestFile1.js
+++ src/TestFile1.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export const TestFile1 = () => {
-  return <div>TestFile1</div>;
+  return (
+    <div>
+      TestFile1
+      <div>TestFile1</div>;<div>TestFile1</div>;<div>TestFile1</div>;
+    </div>
+  );
 };
