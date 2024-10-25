0e1ccf398aadd59c4db99a865e2717c5e22599d3
neha <nehakatekar230@gmail.com>
2024-10-25 14:56:04 +0530

    squash2

--- src/RebaseDemo.js
+++ src/RebaseDemo.js
@@ -1,7 +1,10 @@
-import React from 'react'
+import React from "react";
 
 export const RebaseDemo = () => {
   return (
-    <div>RebaseDemo</div>
-  )
-}
+    <div>
+      RebaseDemo<>rtgf</>
+      <h1>ddddedfrgthyuji7j6hyt</h1>
+    </div>
+  );
+};
